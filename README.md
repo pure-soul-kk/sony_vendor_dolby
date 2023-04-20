8405073f6cf964c68a21a58bc06eac10f5714f56
unsatifsed27 <unsatifsederror@gmail.com>
2023-04-20 16:35:07 +0700

    dolby: initial import dolby

---
+++ README.md
@@ -0,0 +1,13 @@
+clone to vendor/dolby
+
+Add this in your device.mk or common.mk:
+
+```
+# Sony Dolby
+$(call inherit-product, vendor/dolby/config.mk)
+```
+
+
+```
+https://github.com/danhancach/device_sony_pdx206/commit/ebafd0edde1e30073748c6aa8d4243d003620ec3
+```
---
+++ config.mk
@@ -0,0 +1,14 @@
+# Automatically generated file. DO NOT MODIFY
+
+$(call inherit-product, vendor/dolby/dolby.mk)
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.feature.dolby_enable=true \
+	vendor.audio.dolby.ds2.enabled=false \
+	vendor.audio.dolby.ds2.hardbypass=false \
+	ro.audio.monitorRotation=true
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    vendor/dolby/sepolicy/vendor
